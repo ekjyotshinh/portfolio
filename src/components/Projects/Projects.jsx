96ca6d91a76c5ce88f8dbfcbe1556f91390b68a9
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-07-19 11:31:25 -0700

    project updates

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,9 +6,19 @@ const projectList = [
     title: 'ChemTrack Mobile App',
     description:
       'An inventory tracking app with QR scanning, role-based access, and push/email notifications. Built using React Native, Go, and Firestore.',
-    tech: ['React Native', 'Go', 'Firestore', 'GCS', 'CI/CD', 'TypeScript','Testing'],
-    link: 'https://github.com/ekjyotshinh/ChemTrack'
+    tech: ['React Native', 'Go', 'Firestore', 'GCS', 'CI/CD', 'TypeScript'],
+    link: 'https://github.com/ekjyotshinh/ChemTrack',
+    demo: 'https://imgur.com/a/chemtrack-demo-h9Tlx1o'
   },
+
+  {
+    title: 'Gold Rate Website – Sarafa Association Bathinda',
+    description:
+      'Developed an official website for the Sarafa Association Bathinda to publish daily gold and silver rates. Features include real-time updates, admin dashboard, and archive access. Used daily by local vendors and customers.',
+    tech: ['React', 'TypeScript', 'Firebase', 'GCS', 'GCR'],
+    live: 'https://sarafaassociationbathinda.com/' 
+  },
+
   {
     title: 'Redis-like Key-Value Store',
     description:
@@ -16,13 +26,25 @@ const projectList = [
     tech: ['Python', 'Sockets', 'Multithreading'],
     link: 'https://github.com/ekjyotshinh/Lightweight-Redis-clone'
   },
+
   {
-    title: 'Parallel Branch & Bound Solver',
+    title: 'Youtube Filter Extension',
     description:
-      'Optimized load balancing for SOP in C++ using OpenMP, reducing runtime by 15% for large datasets.',
-    tech: ['C++', 'Parallel Algorithms', 'Multithreading'],
-    link: 'https://github.com/jacobnormington/SOP_Solver'
+      'A chrome extension extension that lets you filter out unwanted videos on youtube based on Keywords, Channel names, Subscription status, Mixes or Shorts content',
+    tech: ['JavaScript', 'HTML', 'CSS', 'Manifest V3'],
+    live: 'https://chromewebstore.google.com/detail/youtube-filter/illdholaaiimmiikblhejpkoplibhhjo'
   },
+
+  {
+    title: 'Pharma Study Simulator',
+    description:
+      'Lead a team of 7 to build a secure web application to monitor patient progress and viral loads for Vendia Inc',
+    tech: ['React', 'Firebase', 'Node.js', 'JavaScript'],
+    link: 'https://github.com/Runtime-Terror-131/Demo'
+  },
+
+
+
 
 ]
 
@@ -40,9 +62,29 @@ function Projects() {
                 <li key={i}>{t}</li>
               ))}
             </ul>
-            <a href={project.link} className="project-link" target="_blank" rel="noreferrer">
-              View Code →
-            </a>
+            {project.live && (
+              <a
+                href={project.live}
+                className="project-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {project.title.includes('Extension') ? 'Checkout Extension →' : 'View Website →'}
+              </a>
+            )}
+
+            {project.link && (
+              <a href={project.link} className="project-link" target="_blank" rel="noreferrer">
+                View Code →
+              </a>
+            )}
+
+            {project.demo && (
+              <a href={project.demo} className="project-link" target="_blank" rel="noreferrer">
+                View Demo →
+              </a>
+            )}
+
           </div>
         ))}
       </div>
