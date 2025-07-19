2caa69106d31999047c45a47edc48a97f987ff64
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-07-19 12:00:13 -0700

    link update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,20 @@
 import './Hero.css'
 import SectionContainer from '../Layout/SectionContainer'
+
 function Hero() {
   return (
     <SectionContainer id="hero" className="hero-bg">
       <div className="hero-content">
         <h1>Hello, I'm <span className="highlight">Ekjyot</span></h1>
-        <p>I'm a software developer passionate about building clean and scalable applications.</p>
-        <a href="#projects" className="hero-btn">Resume</a> // TODO : update link to point to the resume
+        <p>I'm a software developer passionate about developing scalable applications.</p>
+        <a
+          href="https://drive.google.com/file/d/1ifi4uxJcXVwBR5o7irkyWukOUk4Jl0ZC/view?usp=drive_link"
+          className="hero-btn"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Resume
+        </a>
       </div>
     </SectionContainer>
   )
