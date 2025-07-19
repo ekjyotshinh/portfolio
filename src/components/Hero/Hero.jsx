48a1327341785bc9243476c87f901f49a12d98de
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-07-18 20:08:50 -0700

    updated hero design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className="hero-content">
         <h1>Hello, I'm <span className="highlight">Ekjyot</span></h1>
         <p>I'm a software developer passionate about building clean and scalable applications.</p>
-        <a href="#projects" className="hero-btn">View Projects</a>
+        <a href="#projects" className="hero-btn">Resume</a> // TODO : update link to point to the resume
       </div>
     </SectionContainer>
   )
