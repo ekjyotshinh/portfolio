59c0f1afe466cfa82b76b1f68d24fde06e5d705a
Ekjyot Singh Shinh <115907177+ekjyotshinh@users.noreply.github.com>
2025-07-18 19:31:39 -0700

    added a container to seperate sections

---
+++ src/components/Layout/SectionContainer.jsx
@@ -0,0 +1,13 @@
+import './SectionContainer.css'
+
+function SectionContainer({ id, children, className = '' }) {
+  return (
+    <section id={id} className={`section ${className}`}>
+      <div className="section-inner">
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default SectionContainer
